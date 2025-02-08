7a49f1afdccc0c12142375cdc4a887bd3463cac4
web-collab <tanmoy587@gmail.com>
2025-02-08 08:58:07 +0530

    Data Mapping with key display the data in browser

--- src/components/Mapping_With_Unique_Id/MappingWithUniqueId.js
+++ src/components/Mapping_With_Unique_Id/MappingWithUniqueId.js
@@ -4,10 +4,10 @@ import Data from './data.json';
 const MappingWithUniqueId=()=>{
   return (
     <div>
-        {Data.map((data)=>{
+        {Data.map((data,index)=>{
             const {title,desc} = data;
             return(
-                <div>
+                <div key={index}>
                     <h2>{title}</h2>
                     <p>{desc}</p>
                 </div>
