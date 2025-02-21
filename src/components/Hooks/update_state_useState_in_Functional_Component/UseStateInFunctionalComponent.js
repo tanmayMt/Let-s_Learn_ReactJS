a4f4f055943b83a3e1c2ff1d2b1ca564dea00202
web-collab <tanmoy587@gmail.com>
2025-02-21 09:15:28 +0530

    update state based on previous state - 'setCount((count)=>count+1);'

--- src/components/Hooks/update_state_useState_in_Functional_Component/UseStateInFunctionalComponent.js
+++ src/components/Hooks/update_state_useState_in_Functional_Component/UseStateInFunctionalComponent.js
@@ -3,12 +3,18 @@ import React,{useState}from 'react'
 import './style.css'
 
 export default function UseStateInFunctionalComponent(){
-  const [count,setCount] = useState(0);
+  const [count,setCount] = useState(0);// Initialize state with count set to 0
 
   const handleIncrement = ()=>{
-    setCount(count+1);  //0+1=1
-    setCount(count+1);  //0+1=1
-    setCount(count+1);  //0+1=1
+    // setCount(count+1);  //0+1=1
+    // setCount(count+1);  //0+1=1
+    // setCount(count+1);  //0+1=1
+    // update state based on previous state
+    setCount((count)=>count+1);  //0+1=1
+    setCount((count)=>count+1);  //1+1=2
+    setCount((count)=>count+1);  //2+1=3
+    //If we directly use setCount(count + 1), React batches state updates, causing unexpected results.
+    // Using a function inside setCount ensures we get the latest state value before updating.
   }
   const handleDecrement = ()=>{
     setCount(count-1);
