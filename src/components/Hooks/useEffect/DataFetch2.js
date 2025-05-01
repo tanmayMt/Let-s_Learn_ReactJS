999c558d0799d287107548476107dec6235d116c
tanmayMt <tanmoy587@gmail.com>
2025-05-01 07:38:15 +0530

    Use of useEffect for data fetching

--- src/components/Hooks/useEffect/DataFetch2.js
+++ src/components/Hooks/useEffect/DataFetch2.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./DataFetch2.css"; // Make sure this CSS file exists
 
+const loadingMessage = <p>todos is loading</p>;
+
 const DataFetch2 = () => {
   const [todos, setTodos] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
+      .then((res) => {
+        if(!res.ok){
+            throw Error("fecthing is not successful");
+        }
+        else{
+            return res.json();
+        }
+      })
       .then((data) => {
         setTodos(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
         setIsLoading(false);
       });
   }, []);
@@ -25,7 +40,9 @@ const DataFetch2 = () => {
   return (
     <div className="todo-container">
       <h1 className="todo-title">Todos</h1>
-      {isLoading ? <p className="loading-text">Loading...</p> : todosElement}
+      {error && <p> {error} </p>}
+      {isLoading && loadingMessage}
+      {todosElement}
     </div>
   );
 };
