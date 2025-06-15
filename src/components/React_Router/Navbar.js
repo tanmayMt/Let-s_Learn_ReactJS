495c521a72fe1537f56bfda0541b84cbcdf69830
Tanmay Samanta <tanmoy587d@gmail.com>
2025-06-15 18:28:42 +0530

    routing with navigation bar - using <NavLink>

--- src/components/React_Router/Navbar.js
+++ src/components/React_Router/Navbar.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-import Home from "./pages/Home";
-import About from "./pages/About";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/" >Home</Link>
-      <Link to="/about" >About</Link>
+      <NavLink to="/" className="nav-link">Home</NavLink>
+      <NavLink to="/about" className="nav-link">About</NavLink>
     </nav>
   )
 }
