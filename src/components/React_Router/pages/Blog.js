bb47df9b34122651f430420436be8c9246d5ad52
Tanmay Samanta <tanmoy587d@gmail.com>
2025-06-16 08:29:17 +0530

    Dynamic Routing - useLocation()

--- src/components/React_Router/pages/Blog.js
+++ src/components/React_Router/pages/Blog.js
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import BlogsData from './BlogsData';
 
 const Blog = () => {
   const {id} = useParams();
+  const location = useLocation();
+  console.log(location);
 
 //  useParams is a React Router hook that lets you access the dynamic parts of the URL (called route parameters).
 // 🟢 It’s used when your route contains something like /user/:id or /product/:slug.
 // You can then extract that id or slug inside the component using useParams.
-  const [bodyData,setBodyData] = useState("");
-  const [titleData,setTitleData] = useState("");
-  useEffect(()=>{
-    //const blogData = BlogsData.filter((blog)=>blog.id===id);
-    const blogData = BlogsData.find((blog) => blog.id === id); // ✅ use .find instead of .filter
-    if(blogData){
-      setBodyData(blogData.body);
-      setTitleData(blogData.title);
-    }
-    else{
-      setBodyData('Blog not found.');
-      setTitleData('404');
-    }
+  // const [bodyData,setBodyData] = useState("");
+  // const [titleData,setTitleData] = useState("");
+  // useEffect(()=>{
+  //   //const blogData = BlogsData.filter((blog)=>blog.id===id);
+  //   const blogData = BlogsData.find((blog) => blog.id === id); // ✅ use .find instead of .filter
+  //   if(blogData){
+  //     setBodyData(blogData.body);
+  //     setTitleData(blogData.title);
+  //   }
+  //   else{
+  //     setBodyData('Blog not found.');
+  //   }
 
-  },[id])
+  // },[id])
   return (
     <div>
-      <h1>{titleData}</h1>
-      <p>{bodyData}</p>
+      <h1>{location.state.title}</h1>
+      <p>{location.state.body}</p>
     </div>
   )
 }
--- src/components/React_Router/pages/Blogs.js
+++ src/components/React_Router/pages/Blogs.js
@@ -32,7 +32,7 @@ const Blogs = () => {
           return <article key={id}>
             <h3>{title}</h3>
             <p>{truncateString(body,90)}</p>
-            <Link to={`/blogs/${id}`}>Learn More</Link>
+            <Link to={`/blogs/${id}`} state={{id,title,body}}>Learn More</Link>
             {/* <Link to={`/blogs/:id`}>Learn More</Link> */}
           </article>
         })}
